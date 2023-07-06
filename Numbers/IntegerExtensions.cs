2326412abecfe1762a7bf05e7bb58218e177f620
Dmitry Nesterenko <trapwhiteclown@gmail.com>
2023-07-06 17:54:07 +0300

    Type of sequence!

--- Numbers/IntegerExtensions.cs
+++ Numbers/IntegerExtensions.cs
@@ -14,7 +14,61 @@ namespace Numbers
         /// or null if the information is not defined.</returns>
         public static ComparisonSigns? GetTypeComparisonSigns(this long number)
         {
-            throw new NotImplementedException();
+            if (number / 10 == 0)
+            {
+                return null;
+            }
+
+            if (number == long.MinValue)
+            {
+                return ComparisonSigns.LessThan | ComparisonSigns.Equals | ComparisonSigns.MoreThan;
+            }
+
+            long[] array = new long[64];
+            int n = 63;
+            while (number > 9)
+            {
+                array[n] = number % 10;
+                n--;
+                number /= 10;
+            }
+
+            array[n] = number;
+            bool less = false, eq = false, more = false;
+
+            for (; n < 63; n++)
+            {
+                if (array[n] < array[n + 1])
+                {
+                    less = true;
+                }
+                else if (array[n] > array[n + 1])
+                {
+                    more = true;
+                }
+                else
+                {
+                    eq = true;
+                }
+            }
+
+            ComparisonSigns result = 0;
+            if (less)
+            {
+                result = result | ComparisonSigns.LessThan;
+            }
+
+            if (more)
+            {
+                result = result | ComparisonSigns.MoreThan;
+            }
+
+            if (eq)
+            {
+                result = result | ComparisonSigns.Equals;
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -24,7 +78,39 @@ namespace Numbers
         /// <returns>The information in the form of a string about the type of sequence that the digit of a given number represents.</returns>
         public static string GetTypeOfDigitsSequence(this long number)
         {
-            throw new NotImplementedException();
+            ComparisonSigns? result = GetTypeComparisonSigns(number);
+
+            if (result == null)
+            {
+                return "One digit number.";
+            }
+
+            if ((int)result == 1)
+            {
+                return "Strictly Increasing.";
+            }
+
+            if ((int)result == 2)
+            {
+                return "Strictly Decreasing.";
+            }
+
+            if ((int)result == 4)
+            {
+                return "Monotonous.";
+            }
+
+            if ((int)result == 5)
+            {
+                return "Increasing.";
+            }
+
+            if ((int)result == 6)
+            {
+                return "Decreasing.";
+            }
+
+            return "Unordered.";
         }
     }
 }
